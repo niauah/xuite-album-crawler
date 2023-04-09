1607a400f3234490aec4337fdabcfe57424373f6
niauah <khengiong@ithuan.tw>
2023-04-09 14:28:58 +0800

    Use while loop to expand the full album list

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+from selenium.common.exceptions import StaleElementReferenceException
 
 driver = webdriver.Firefox()
 driver.get("https://m.xuite.net/photo/zooz103")
 more_button = driver.find_element(By.CLASS_NAME, 'albumlist-more')
-print(more_button)
 
-more_button.click()
-more_button = driver.find_element(By.CLASS_NAME, 'albumlist-more')
-print(more_button)
+while True:
+	try:
+		more_button.click()
+		print('Clicked the "more" button')
+	except StaleElementReferenceException as e:
+		print('The "more" button is stale, reached the end of album list.')
+		break
+
 driver.close()
