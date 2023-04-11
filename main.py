106e150c89e97233d072cfdbdf05fc19c30ee7b8
niauah <khengiong@ithuan.tw>
2023-04-11 17:24:11 +0800

    Import

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import StaleElementReferenceException, NoSuchElementException
+from selenium.common.exceptions import (
+	StaleElementReferenceException, NoSuchElementException, InvalidSessionIdException
+)
 
 import re
 import os
